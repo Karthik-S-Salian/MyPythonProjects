a975d976263b9412e8142e941a843bc764892609
Karthik-S-Salian <karthikssalian5@gmail.com>
2022-07-11 19:39:38 +0530

    11/7 commit

--- Web Projects/social book/social_book/core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Profile(models.Model):
-    user=None
-    id_user=None
-    bio=None
-    profileimg=models.ImageField()
-    location=None
